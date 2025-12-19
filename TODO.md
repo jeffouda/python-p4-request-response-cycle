1e54163b9783e7fb89e1e29173d18cd7bfdb8673
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-12-19 10:58:12 +0300

    Add pytest as dev dependency to enable running tests

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+
+- [x] Install dependencies using `pipenv install`
+- [x] Run tests to verify the fix
+- [x] Fix any logic errors if tests fail
+
--- server/testing/codegrade_test.py
+++ server/testing/codegrade_test.py
@@ -1,4 +1,21 @@
+import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from app import app
+
 
 def test_codegrade_placeholder():
     """Codegrade placeholder test"""
-    assert 1==1
+    assert 1 == 1
+
+
+def test_index_route():
+    """Test the index route returns correct response"""
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert b"The host for this page is" in response.data
+        assert b"The name of this application is" in response.data
+        assert b"The path of this application on the user's device is" in response.data
